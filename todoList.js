0eae1c475c4cbe359fbac90ec7ad830c5ff4ab35
Kailash <56155064+kailash-s@users.noreply.github.com>
2021-02-22 22:14:45 +0000

    Added delete functionality

--- todoList.js
+++ todoList.js
@@ -1,3 +1,22 @@
+var myNodeList = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodeList.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodeList[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
 function newAdd() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
